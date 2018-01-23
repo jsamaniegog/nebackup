2175eedb2d9290eb0ce48ac00076b9449a3fcd8d
jsamaniegog <jsamaniegog@gmail.com>
2018-01-23 09:20:12 +0100

    Update version to 2.2.0

--- setup.php
+++ setup.php
@@ -109,7 +109,7 @@ function plugin_init_nebackup() {
  */
 function plugin_version_nebackup() {
     return array('name' => 'nebackup',
-        'version' => '2.1.3',
+        'version' => '2.2.0',
         'author' => 'Javier Samaniego',
         'license' => 'AGPLv3+',
         'homepage' => 'https://github.com/jsamaniegog/nebackup',
